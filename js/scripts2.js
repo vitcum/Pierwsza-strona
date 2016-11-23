2525ce29ea6075b4f31b6b924861f61f59616f86
vitcum <vitcum@gmail.com>
2016-11-23 12:34:52 +0100

    Add files via upload

---
+++ js/scripts2.js
@@ -0,0 +1,5 @@
+var a = 5
+var h = 10
+var triangleArea = a*h/2
+alert('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
