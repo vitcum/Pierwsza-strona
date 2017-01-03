977d8952656692999f5fc6ef08152ee1c83ab104
vitcum <vitcum@gmail.com>
2017-01-03 14:13:58 +0100

    Delete scripts2.js

--- js/scripts2.js
+++
@@ -1,5 +0,0 @@
-var a = 5
-var h = 10
-var triangleArea = a*h/2
-alert('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
-console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
